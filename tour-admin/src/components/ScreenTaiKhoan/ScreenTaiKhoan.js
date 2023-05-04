982e52f716230d8780c1b88813fdd5885d549545
Dat <datnguyen9g@gmail.com>
2023-05-04 19:26:58 +0700

    thêm validation cho screen tai khoan

--- tour-admin/src/components/ScreenTaiKhoan/ScreenTaiKhoan.js
+++ tour-admin/src/components/ScreenTaiKhoan/ScreenTaiKhoan.js
@@ -17,6 +17,33 @@ export default function ScreenTaiKhoan(props) {
     const [isUpdatePopup, setIsUpdatePopup] = useState(false)
     const [isErrorUpdate, setIsErrorUpdate] = useState(false)
     const [isUpdateMatKhauPopup, setIsUpdateMatKhauPopup] = useState(false)
+    // thông tin validate 
+    const [validateTen, setValidateTen] = useState({
+        "status": false,
+        "content": ""
+    })
+    const [validateDiaChi, setValidateDiaChi] = useState({
+        "status": false,
+        "content": ""
+    })
+    const [validateSdt, setValidateSdt] = useState({
+        "status": false,
+        "content": ""
+    })
+    // thong tin validate password
+    const [validateOldPassword, setValidateOldPassword] = useState({
+        "status": false,
+        "content": ""
+    })
+    const [validateNewPassword, setValidateNewPassword] = useState({
+        "status": false,
+        "content": ""
+    })
+    const [validateReNewPassword, setValidateReNewPassword] = useState({
+        "status": false,
+        "content": ""
+    })
+
     // thông tin người dùng
     const [ten, setTen] = useState(nguoiDung.ten)
     const [diaChi, setDiaChi] = useState(nguoiDung.diaChi)
@@ -28,13 +55,58 @@ export default function ScreenTaiKhoan(props) {
         setIsUpdatePopup(!isUpdatePopup)
     }
     const onChangeOldPassword = (e) => {
-        setOldPassword(e.target.value)
+        let opw = e.target.value
+        setOldPassword(opw)
+        let validPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+        if (!opw.match(validPassword)) {
+            setValidateOldPassword({
+                "status": true,
+                "content": "Phải chứa ít nhất một số và một chữ hoa và chữ thường và ít nhất 8 ký tự trở lên"
+            })
+        } else {
+            setValidateOldPassword({
+                "status": false,
+                "content": "correct!"
+            })
+        }
     }
     const onChangeNewPassword = (e) => {
-        setNewPassword(e.target.value)
+        let npw = e.target.value
+        setNewPassword(npw)
+        let validPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+        if (!npw.match(validPassword)) {
+            setValidateNewPassword({
+                "status": true,
+                "content": "Phải chứa ít nhất một số và một chữ hoa và chữ thường và ít nhất 8 ký tự trở lên"
+            })
+        } else {
+            setValidateNewPassword({
+                "status": false,
+                "content": "correct!"
+            })
+        }
     }
     const onChangeRenewPassword = (e) => {
-        setRenewPassword(e.target.value)
+        let rnpw = e.target.value
+        setRenewPassword(rnpw)
+        let validPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+        if (newPassword != rnpw) {
+            setValidateReNewPassword({
+                "status": true,
+                "content": "Nhập lại mật khẩu chưa chùng khớp!"
+            })
+        }
+        else if (!rnpw.match(validPassword)) {
+            setValidateReNewPassword({
+                "status": true,
+                "content": "Phải chứa ít nhất một số và một chữ hoa và chữ thường và ít nhất 8 ký tự trở lên"
+            })
+        } else {
+            setValidateReNewPassword({
+                "status": false,
+                "content": "correct!"
+            })
+        }
     }
     useEffect(() => {
         setNguoiDung(props.nguoiDung)
@@ -127,6 +199,54 @@ export default function ScreenTaiKhoan(props) {
         setDiaChi("")
         setSdt("")
     }
+    const handleChangeTen = (e) => {
+        let new_ten = e.target.value
+        setTen(new_ten)
+        let validRegex = /[^A-e]/g;
+        if (!new_ten.match(validRegex)) {
+            setValidateTen({
+                "status": true,
+                "content": "Không được bỏ trống Tên"
+            })
+        } else {
+            setValidateTen({
+                "status": false,
+                "content": "correct!"
+            })
+        }
+    }
+    const handleChangeDC = (e) => {
+        let new_dc = e.target.value
+        setDiaChi(new_dc)
+        let validRegex = /[^A-e]/g;
+        if (!new_dc.match(validRegex)) {
+            setValidateDiaChi({
+                "status": true,
+                "content": "Không được bỏ trống Địa chỉ"
+            })
+        } else {
+            setValidateDiaChi({
+                "status": false,
+                "content": "correct!"
+            })
+        }
+    }
+    const handleChangeSDT = (e) => {
+        let new_sdt = e.target.value
+        setSdt(new_sdt)
+        let validRegex = /^[0-9]{10}$/;
+        if (!new_sdt.match(validRegex)) {
+            setValidateSdt({
+                "status": true,
+                "content": "Không được bỏ trống Địa chỉ"
+            })
+        } else {
+            setValidateSdt({
+                "status": false,
+                "content": "correct!"
+            })
+        }
+    }
     return (
         <div className='tai-khoan-container'>
             <div className='thongke-header'>
@@ -149,11 +269,33 @@ export default function ScreenTaiKhoan(props) {
                                     <p>Mã (ID) * : {nguoiDung.document_id}</p>
                                     <p>Email {"(Email)"}*: {email}</p>
                                     <p>Tên người dùng {"(Full Name)"}*: {ten}</p>
-                                    <input type='text' onChange={(e) => setTen(e.target.value)} value={ten} />
+                                    <div className='group-input' style={{ flexDirection: "row", display: "flex" }}>
+                                        <input type='text' onChange={(e) => handleChangeTen(e)} value={ten} />
+                                        {
+                                            validateTen.content != "" ? (
+                                                validateTen.status ? <p style={{ color: "red", marginLeft: 10 }}> {validateTen.content}</p> : <p style={{ color: "green", marginLeft: 10 }}>correct!</p>
+                                            ) : ""
+                                        }
+                                    </div>
                                     <p>Địa chỉ {"(Address)"}*: {diaChi}</p>
-                                    <input type='text' onChange={(e) => setDiaChi(e.target.value)} value={diaChi} />
+                                    <div className='group-input' style={{ flexDirection: "row", display: "flex" }}>
+                                        <input type='text' onChange={(e) => handleChangeDC(e)} value={diaChi} />
+
+                                        {
+                                            validateDiaChi.content != "" ? (
+                                                validateDiaChi.status ? <p style={{ color: "red", marginLeft: 10 }}> {validateDiaChi.content}</p> : <p style={{ color: "green", marginLeft: 10 }}>correct!</p>
+                                            ) : ""
+                                        }
+                                    </div>
                                     <p>Điện thoại {"(Phone number)"}*: {sdt}</p>
-                                    <input type='text' onChange={(e) => setSdt(e.target.value)} value={sdt} />
+                                    <div className='group-input' style={{ flexDirection: "row", display: "flex" }}>
+                                        <input type='text' onChange={(e) => handleChangeSDT(e)} value={sdt} />
+                                        {
+                                            validateSdt.content != "" ? (
+                                                validateSdt.status ? <p style={{ color: "red", marginLeft: 10 }}> {validateSdt.content}</p> : <p style={{ color: "green", marginLeft: 10 }}>correct!</p>
+                                            ) : ""
+                                        }
+                                    </div>
                                     <hr />
                                     <div>
                                         <Button variant="outline-secondary" onClick={() => handleRespaceUser()}>Xóa trắng</Button>{' '}
@@ -197,23 +339,47 @@ export default function ScreenTaiKhoan(props) {
                                 <div>
                                     <p>ID tài khoản (document_id) * : {nguoiDung.email}</p>
                                     <p>Mật khẩu cũ (Older Password) * : </p>
-                                    <input
-                                        type='password'
-                                        placeholder='Aa87654321'
-                                        onChange={e => onChangeOldPassword(e)}
-                                        value={oldPassword} />
+                                    <div className='group-input' style={{ flexDirection: "row", display: "flex" }}>
+                                        <input
+                                            type='password'
+                                            placeholder='Aa87654321'
+                                            onChange={e => onChangeOldPassword(e)}
+                                            value={oldPassword} />
+                                        {
+                                            validateOldPassword.content != "" ? (
+                                                validateOldPassword.status ? <p style={{ color: "red", marginLeft: 10 }}> {validateOldPassword.content}</p> : <p style={{ color: "green", marginLeft: 10 }}>correct!</p>
+                                            ) : ""
+                                        }
+                                    </div>
+
                                     <p>Mật khẩu mới {"(New Password)"}*:</p>
-                                    <input
-                                         placeholder='Aa12345678'
-                                        type='password'
-                                        onChange={e => onChangeNewPassword(e)}
-                                        value={newPassword} />
+                                    <div className='group-input' style={{ flexDirection: "row", display: "flex" }}>
+                                        <input
+                                            placeholder='Aa12345678'
+                                            type='password'
+                                            onChange={e => onChangeNewPassword(e)}
+                                            value={newPassword} />
+                                        {
+                                            validateNewPassword.content != "" ? (
+                                                validateNewPassword.status ? <p style={{ color: "red", marginLeft: 10 }}> {validateNewPassword.content}</p> : <p style={{ color: "green", marginLeft: 10 }}>correct!</p>
+                                            ) : ""
+                                        }
+                                    </div>
+
                                     <p>Nhập lại mật khẩu mới {"(New Password)"}*:</p>
-                                    <input
-                                        placeholder='Aa12345678'
-                                        type='password'
-                                        onChange={e => onChangeRenewPassword(e)}
-                                        value={renewPassword} />
+                                    <div className='group-input' style={{ flexDirection: "row", display: "flex" }}>
+                                        <input
+                                            placeholder='Aa12345678'
+                                            type='password'
+                                            onChange={e => onChangeRenewPassword(e)}
+                                            value={renewPassword} />
+                                        {
+                                            validateReNewPassword.content != "" ? (
+                                                validateReNewPassword.status ? <p style={{ color: "red", marginLeft: 10 }}> {validateReNewPassword.content}</p> : <p style={{ color: "green", marginLeft: 10 }}>correct!</p>
+                                            ) : ""
+                                        }
+                                    </div>
+
                                     <hr />
                                     <div>
                                         <Button variant="outline-secondary" onClick={() => handleRespacePW()}>Xóa trắng</Button>{' '}
